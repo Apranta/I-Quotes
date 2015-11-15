5054d074d7a22ab192bb05e92a2f06fc5fb607e4
Rezi Apriliansyah <Rezi Apriliansyah>
2015-11-15 11:12:22 +0700

    ubah koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "quotes";
+$host = "ap-cdbr-azure-southeast-a.cloudapp.net";
+$user = "b6a18f25b309d1";
+$pass = "bd9a8d78";
+$db = "rezidb";
 $konek = mysql_connect($host, $user, $pass) or die ('Koneksi Gagal! ');
 mysql_select_db($db);
 ?>
